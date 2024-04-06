7ef59dc8e8958e873707c71db0972879954d6e08
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-06 21:32:17 +0300

    Update Ethio-Gebeta.cpp

--- Ethio-Gebeta.cpp
+++ Ethio-Gebeta.cpp
@@ -4699,3 +4699,155 @@ void MR_VoicePack()
         }
     }
 }
+void MR_DataPack()
+{
+    int x;
+    cout << "\n**********MONTHLY RECURRING INTERNET PACKAGE**********" << endl;
+    cout << "\t1.Enter '40' Birr 40 for 500 MB" << endl;
+    cout << "\t2.Enter '75' Birr 75 for 1 GB" << endl;
+    cout << "\t3.Enter '130' Birr 130 for 2 GB" << endl;
+    cout << "\t4.Enter '250' Birr 250 for 4 GB" << endl;
+    cout << "\t\tTo go one  step back Enter '1'" << endl;
+    cout << "\t\tTo go two  steps back Enter '2'" << endl;
+    cout << "\t\tTo go three  steps back Enter '3'" << endl;
+    cout << "\t\tTo go back to the first menu Enter '0'" << endl;
+    cout << "Option: ";
+    cin >> x;
+    system("CLS");
+    if (y - x >= 0)
+    {
+        if (x == 40)
+        {
+            cout << "\n\t*************Congratulations!!! You Have Successfully Bought Your Monthly Recurring Internet Package************************" << endl;
+            cout << "\tDear customer, as per your request the new service offer \n Monthly  Data Package 500 MB is added to your service number. the service offer is effective from 12:00 am to 6:00 " << endl;
+            cout << "\t\tYour Balance is now " << y - 40 << " Birr" << endl;
+            int d;
+            cout << "\n1.Enter '0' to buy another package\n2.Enter any key for 'EXIT'" << endl;
+            cin >> d;
+            switch (d)
+            {
+            case 0:
+                Firstmenu();
+                break;
+            default:
+                break;
+            }
+        }
+        else if (x == 75)
+        {
+            cout << "\n\tCongratulations!!! You Have Successfully Bought Your Recurring Internet Package" << endl;
+            cout << "\tDear customer, as per your request the new service offer \n Monthly Data Package 1 GB is added to your service number. the service offer is effective from 12:00 am to 6:00 " << endl;
+            cout << "\t\tYour Balance is now " << y - 75 << " Birr" << endl;
+            int d;
+            cout << "\n1.Enter '0' to buy another package\n2.Enter any key for 'EXIT'" << endl;
+            cin >> d;
+            switch (d)
+            {
+            case 0:
+                Firstmenu();
+                break;
+            default:
+                break;
+            }
+        }
+        else if (x == 130)
+        {
+            cout << "\n\t*************Congratulations!!! You Have Successfully Bought Your Monthly Recurring Internet Package************************" << endl;
+            cout << "\tDear customer, as per your request the new service offer \n Monthly Data Package 2 GB is added to your service number. the service offer is effective from 12:00 am to 6:00 " << endl;
+            cout << "\t\tYour Balance is now " << y - 130 << " Birr" << endl;
+            int d;
+            cout << "\n1.Enter '0' to buy another package\n2.Enter any key for 'EXIT'" << endl;
+            cin >> d;
+            switch (d)
+            {
+            case 0:
+                Firstmenu();
+                break;
+            default:
+                break;
+            }
+        }
+        else if (x == 250)
+        {
+            cout << "\n\t***************Congratulations!!! You Have Successfully Bought Your Monthly Recurring Internet Package************************" << endl;
+            cout << "\tDear customer, as per your request the new service offer \n Monthly Data Package 4 GB is added to your service number. the service offer is effective from 12:00 am to 6:00 " << endl;
+            cout << "\t\tYour Balance is now " << y - 250 << " Birr" << endl;
+            int d;
+            cout << "\n1.Enter '0' to buy another package\n2.Enter any key for 'EXIT'" << endl;
+            cin >> d;
+            switch (d)
+            {
+            case 0:
+                Firstmenu();
+                break;
+            default:
+                break;
+            }
+        }
+
+        else if (x == 1)
+        {
+            recuring();
+        }
+        else if (x == 2)
+        {
+            ForYourself();
+        }
+        else if (x == 3)
+        {
+            services();
+        }
+        else if (x == 0)
+        {
+            Firstmenu();
+        }
+        else
+        {
+            cout << "__________Wrong input, please try again.__________" << endl;
+            MR_DataPack();
+        }
+    }
+    else
+    {
+        int c;
+        cout << "\n\tDear user your Balance is insufficient for this service. " << endl;
+        cout << "\t\tTo get CREDIT Please Enter '0': ";
+        cin >> c;
+        system("CLS");
+        if (c == 0)
+        {
+            credit();
+        }
+        else
+        {
+            cout << "\n\t\t\tTo go one  step back Enter '*'" << endl;
+            cout << "\t\t\tTo go two  steps back Enter '**'" << endl;
+            cout << "\t\t\tTo go three  steps back Enter '***'" << endl;
+            cout << "\t\t\tTo go back to the first menu Enter '0'" << endl;
+            cout << "Option: ";
+            cin >> x;
+            system("CLS");
+            if (x == 1)
+            {
+                recuring();
+            }
+            else if (x == 2)
+            {
+                ForYourself();
+            }
+            else if (x == 3)
+            {
+                services();
+            }
+            else if (x == 0)
+            {
+                Firstmenu();
+            }
+            else
+            {
+                cout << "__________Wrong input, please try again.__________" << endl;
+                MR_DataPack();
+            }
+        }
+    }
+}
