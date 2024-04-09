7753d03979f1ffd64215db481cb2e67c8607c128
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-09 21:28:11 +0300

    adding Unlimited_Voice_Gift_Package

--- Ethio-Gebeta.cpp
+++ Ethio-Gebeta.cpp
@@ -9818,3 +9818,179 @@ void Unlimited_Data_Gift_Package()
         }
     }
 }
+void Unlimited_Voice_Gift_Package()
+{
+    int x;
+    cout << "\n*******************************************" << endl;
+    cout << "**********UNLIMITED VOICE GIFT PACKAGE**********" << endl;
+    cout << "*******************************************" << endl;
+    cout << "\t\t 1.Enter' Weekly unlimited Voice Package:" << endl;
+    cout << " Birr 440-->With Free Unlimited SMS" << endl;
+    cout << "\t\t 2.Enter '836' Bi-Weekly unlimited Voice Package" << endl;
+    cout << " \tBirr 836-->With Free Unlimited SMS" << endl;
+    cout << "\t\t 3.Enter '1510' Monthly unlimited Voice Package:" << endl;
+    cout << " \tBirr 1510-->With Free Unlimited SMS" << endl;
+    cout << "\tTo go one  step back Enter '1'" << endl;
+    cout << "\tTo go two  steps back Enter '2'" << endl;
+    cout << "\tTo go three  steps back Enter '3'" << endl;
+    cout << "\tTo go back to the first menu Enter '0'" << endl;
+    cout << "Option: ";
+    cin >> x;
+    system("CLS");
+    if (y - x >= 0)
+    {
+        if (x == 440)
+        {
+            int num;
+            cout << "Please Provide the Phone number you are sending to" << endl;
+            cout << "+251 ";
+            cin >> num;
+            system("CLS");
+            if (num <= 999999999 && num >= 900000000)
+            {
+                cout << "you have successfully sent a gift to " << num << endl;
+                cout << "Your Balance is now " << y - 440 << " Birr" << endl;
+                int d;
+                cout << "\n1.Enter '0' to buy another package\n2.Enter any key for 'EXIT'" << endl;
+                cin >> d;
+                switch (d)
+                {
+                case 0:
+                    Firstmenu();
+                    break;
+                default:
+                    break;
+                }
+            }
+            else
+            {
+                cout << "Sorry this Number does not Exist, Please try again" << endl;
+                Unlimited_Voice_Gift_Package();
+            }
+        }
+        else if (x == 836)
+        {
+            int num;
+            cout << "Please Provide the Phone number you are sending to" << endl;
+            cout << "+251 ";
+            cin >> num;
+            system("CLS");
+            if (num <= 999999999 && num >= 900000000)
+            {
+                cout << "you have successfully sent a gift to " << num << endl;
+                cout << "Your Balance is now " << y - 836 << " Birr" << endl;
+                int d;
+                cout << "\n1.Enter '0' to buy another package\n2.Enter any key for 'EXIT'" << endl;
+                cin >> d;
+                switch (d)
+                {
+                case 0:
+                    Firstmenu();
+                    break;
+                default:
+                    break;
+                }
+            }
+            else
+            {
+                cout << "Sorry this Number does not Exist, Please try again" << endl;
+                Unlimited_Voice_Gift_Package();
+            }
+        }
+        else if (x == 1510)
+        {
+            int num;
+            cout << "Please Provide the Phone number you are sending to" << endl;
+            cout << "+251 ";
+            cin >> num;
+            system("CLS");
+            if (num <= 999999999 && num >= 900000000)
+            {
+                cout << "you have successfully sent a gift to " << num << endl;
+                cout << "Your Balance is now " << y - 1510 << " Birr" << endl;
+                int d;
+                cout << "\n1.Enter '0' to buy another package\n2.Enter any key for 'EXIT'" << endl;
+                cin >> d;
+                switch (d)
+                {
+                case 0:
+                    Firstmenu();
+                    break;
+                default:
+                    break;
+                }
+            }
+            else
+            {
+                cout << "Sorry this Number does not Exist, Please try again" << endl;
+                Unlimited_Voice_Gift_Package();
+            }
+        }
+        else if (x == 1)
+        {
+            Premium_Unlimited_Mobile_Gift_Package();
+        }
+        else if (x == 2)
+        {
+            ForOfferingGift();
+        }
+        else if (x == 3)
+        {
+            services();
+        }
+        else if (x == 0)
+        {
+            Firstmenu();
+        }
+        else
+        {
+            cout << "__________Wrong input, please try again.__________" << endl;
+            Unlimited_Voice_Gift_Package();
+        }
+    }
+    else
+    {
+        int c;
+        cout << "\n\tDear user your Balance is insufficient for this service. " << endl;
+        cout << "\t\tTo get CREDIT Please Enter '0': ";
+        cin >> c;
+        system("CLS");
+        if (c == 0)
+        {
+            credit();
+        }
+
+        else
+        {
+            int x;
+            cout << "\n\t\t\tTo go one  step back Enter '*'" << endl;
+            cout << "\t\t\tTo go two  steps back Enter '**'" << endl;
+            cout << "\t\t\tTo go three  steps back Enter '***'" << endl;
+            cout << "\t\t\tTo go back to the first menu Enter '0'" << endl;
+            cout << "Option: ";
+            cin >> x;
+            system("CLS");
+            if (x == 1)
+            {
+                Premium_Unlimited_Mobile_Package();
+            }
+            else if (x == 2)
+            {
+                ForYourself();
+            }
+            else if (x == 3)
+            {
+                services();
+            }
+            else if (x == 0)
+            {
+                Firstmenu();
+            }
+            else
+            {
+                cout << "__________Wrong input, please try again.__________" << endl;
+                Unlimited_Voice_Data_Package();
+            }
+        }
+    }
+}
